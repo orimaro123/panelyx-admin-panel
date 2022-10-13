f818bca7c62ebd676f220ec83cf49ff718bfae69
Ori Marom <ori.marom10@gmail.com>
2022-10-13 14:35:02 +0300

    add routes skeleton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login"
+import List from "./pages/list/List"
+import Single from "./pages/single/single"
+import New from "./pages/new/New"
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-   lama dev
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route pate="login" element={<Login />} />
+            <Route pate="users" >
+              <Route index element={<List/>} />
+              <Route path=":userId" element={<Single/>}/>
+              <Route path="new" element={<New/>}/>
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
