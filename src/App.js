8c9b32494ad35f4534049b91a245c13b814f7b31
Ori Marom <ori.marom10@gmail.com>
2022-10-13 15:03:22 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import Home from "./pages/home/Home";
-import Login from "./pages/login/Login"
-import List from "./pages/list/List"
-import Single from "./pages/single/single"
-import New from "./pages/new/New"
+import Login from "./pages/login/Login";
+import List from "./pages/list/List";
+import Single from "./pages/single/Single";
+import New from "./pages/new/New";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
@@ -13,11 +13,16 @@ function App() {
         <Routes>
           <Route path="/">
             <Route index element={<Home />} />
-            <Route pate="login" element={<Login />} />
-            <Route pate="users" >
+            <Route path="login" element={<Login />} />
+            <Route path="users">
               <Route index element={<List/>} />
-              <Route path=":userId" element={<Single/>}/>
-              <Route path="new" element={<New/>}/>
+              <Route path=":userId" element={<Single />} />
+              <Route path="new" element={<New />} />
+            </Route>
+            <Route path="products">
+              <Route index element={<List/>} />
+              <Route path=":productId" element={<Single />} />
+              <Route path="new" element={<New />} />
             </Route>
           </Route>
         </Routes>
